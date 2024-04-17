0cc3649a97d0ab1b353dbbc72e0755ff0761654f
ZNX <henrique.baronceli@gmail.com>
2024-04-17 16:44:54 +1200

    fix wrappers

--- src/Wrapper/index.ts
+++ src/Wrapper/index.ts
@@ -12,11 +12,9 @@ interface ComponentProps {
 export const AppWrapper = styled.div<ComponentProps>`
   background: ${(props) => props.background || "transparent"};
   background-image: ${(props) => props.backgroundImage || ""};
-  height: 100vh;
   margin: ${(props) => props.margin || "0"};
   overflow: ${(props) => props.margin || "auto"};
   padding: ${(props) => props.margin || "1rem"};
-  width: 100vw;
 `;
 
 export const PageWrapper = styled.div<ComponentProps>`
@@ -25,8 +23,8 @@ export const PageWrapper = styled.div<ComponentProps>`
   height: auto;
   margin: ${(props) => props.margin || "0"};
   max-height: 100%;
-  max-width: 100%;
   overflow: ${(props) => props.margin || "auto"};
   padding: ${(props) => props.margin || "1rem"};
-  width: ${(props) => props.width || "1320px"};
+  width: ${(props) => props.width};
+  max-width: ${(props) => props.width || "1320px"};
 `;
